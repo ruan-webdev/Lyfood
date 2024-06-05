65546f937ca47e3af4707196cf7129e627144cb7
Ruan Campos <ruancamposdemarcos@gmail.com>
2024-06-05 03:12:50 -0300

    feat: font family poppins add

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./**/*.{js,jsx,ts,tsx,html}"],
   theme: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
     extend: {
       backgroundImage: {
         home: "url('/assets/bg.png')",
